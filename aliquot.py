e95d80a60e2c6f5894f78e19f539f26f468d20c2
ideophagous <antontheog@gmail.com>
2018-08-31 18:08:30 +0200

    plotting added
    can plot all subgraphs and save them as png images

--- aliquot.py
+++ aliquot.py
@@ -10,35 +10,39 @@ def getDivisors(n):
     divisors = sorted(list(set(divisors)))
     return divisors
 
-def sum(lis):
+def new_sum(lis):
     ss = 0
     for i in range(len(lis)-1):
         ss+=lis[i]
     return ss
 
-def aliquot(n,end):
+def aliquot(n,end,max_value):
     sequence = [n]
     stop     = False
     counter  = 0
+    flag     = None
     while not stop:
-        m     = sum(getDivisors(n))
+        m    = new_sum(getDivisors(n))
         if m in sequence:
             stop = True
+            flag = 'loop '+str(sequence.index(m))
         elif m == 1:
             sequence.append(1)
             stop = True
-        elif counter == end:
+            flag = 'end'
+        elif counter == end or (max_value is not None and m>max_value):
             stop = True
+            flag = 'interrupted'
         else:
             sequence.append(m)
             n    = m
         counter+=1
-    return sequence
+    return sequence,flag
 
-def mapping(n,end):
+def mapping(n,end,max_value):
     mapping = []
     for i in range(1,n+1):
-        mapping.append(aliquot(i,end))
+        mapping.append(aliquot(i,end,max_value))
     return mapping
 
 def mapping2(n,end):
@@ -49,7 +53,7 @@ def mapping2(n,end):
         stop     = False
         counter  = 0
         while not stop:
-            m     = sum(getDivisors(x))
+            m     = new_sum(getDivisors(x))
             if m in sequence:
                 stop = True
             elif m<x:
@@ -76,28 +80,34 @@ def isContained(g,edge):
             return True
     return False
 
-def agraph(n,end):
-    aliqs = mapping(n,end)
+def agraph(n,end,max_value):
+    aliqs = mapping(n,end,max_value)
 
     g = Graph(n+1)
     for aliq in aliqs:
-        print('gg: '+str(aliq))
-        if len(aliq) == 1:
-            if aliq[0]+1>g.vcount():
-                g.add_vertices(aliq[0]+1-g.vcount())
-            if not isContained(g,(aliq[0],aliq[0])):
-                g.add_edges([(aliq[0],aliq[0])])
+        #print('gg: '+str(aliq))
+        if len(aliq[0]) == 1:
+            if aliq[0][0]+1>g.vcount():
+                g.add_vertices(aliq[0][0]+1-g.vcount())
+            if not isContained(g,(aliq[0][0],aliq[0][0])):
+                g.add_edges([(aliq[0][0],aliq[0][0])])
         else:
-            for i in range(len(aliq)-1):
-                if max(aliq[i],aliq[i+1])+1>g.vcount():
-                    g.add_vertices(max(aliq[i],aliq[i+1])+1-g.vcount())
-                if not isContained(g,(aliq[i],aliq[i+1])):
-                    g.add_edges([(aliq[i],aliq[i+1])])
-            '''if aliq[-1] not in [0,1]:
-                if aliq[-1]+1>g.vcount():
-                    g.add_vertices(aliq[-1]+1-g.vcount())
-                if not isContained(g,(aliq[-1],aliq[-1])):
-                    g.add_edges([(aliq[-1],aliq[-1])])'''
+            for i in range(len(aliq[0])-1):
+                if max(aliq[0][i],aliq[0][i+1])+1>g.vcount():
+                    g.add_vertices(max(aliq[0][i],aliq[0][i+1])+1-g.vcount())
+                if not isContained(g,(aliq[0][i],aliq[0][i+1])):
+                    g.add_edges([(aliq[0][i],aliq[0][i+1])])
+            if aliq[0][-1] not in [0,1]:
+                #print('aliq[0][-1] '+str(aliq[0][-1]))
+                '''if aliq[0][-1]+1>g.vcount():
+                    g.add_vertices(aliq[0][-1]+1-g.vcount())'''
+                if aliq[1] is not None:
+                    flag = aliq[1].split()
+                    if flag[0] == 'loop':
+                        print(aliq[0][-1])
+                        index = int(flag[1])
+                        if not isContained(g,(aliq[0][index],aliq[0][-1])):
+                            g.add_edges([(aliq[0][index],aliq[0][-1])])
     labels = []
     for i in range(g.vcount()):
         labels.append(i)
@@ -109,27 +119,30 @@ def getSignificantParts(g):
     comp = g.clusters()
     clusters = []
     for i in range(len(comp)):
-        if len(comp[i])>1:
-            clusters.append(g.subgraph(comp[i]))
+        cluster = g.subgraph(comp[i])
+        if sum(1 for _ in enumerate(cluster.es))>0:
+            clusters.append(cluster)
     return clusters
         
 
 if __name__=='__main__':
     n   = int(input('Enter some number: '))
     end = int(input('Enter max length of Aliquot sequence: '))
+    max_value = int(input('Enter max value in sequence: '))
     #print(sum(getDivisors(n)))
     #print(aliquot(n,end))
-    aliqs = mapping(n,end)
+    '''aliqs = mapping(n,end,max_value)
     for aliq in aliqs:
-        print(aliq)
-    alg = agraph(n,end)
-    print(alg)
+        print(aliq)'''
+    alg = agraph(n,end,max_value)
+    #print(alg)
     visual_style = {}
     comp = getSignificantParts(alg)
-    print(type(comp))
+    #print(type(comp))
     visual_style["bbox"] = (1500, 1500)
     counter = 1
     for clu in comp:
         gr = plot(clu,**visual_style)
-        gr.save(str(counter)+'.png')
+        gr.save(str(counter)+'_n='+str(n)+'_max_sequence_size='+str(end)+'_max_value='+str(max_value)+'.png')
         counter+=1
+    
